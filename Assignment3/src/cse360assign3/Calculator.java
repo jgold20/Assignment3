cfd129b06cdabb27d917579dd8e4765ec5b2b851
Josh <joshgold20@gmail.com>
2016-02-21 14:39:06 -0700

    test

--- Assignment3/src/cse360assign3/Calculator.java
+++ Assignment3/src/cse360assign3/Calculator.java
@@ -1,34 +1,71 @@
 
 package cse360assign3;
 
+/** Calculator 
+ * @author Joshua Gold
+ * @version 2/21/16
+ * 
+ */
+
 public class Calculator {
 
 	private int total;
 	
+	/** Creates calculator object */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	
+	/** Returns the total
+	 * 
+	 * @return total
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	
+	/** Adds a value to the total
+	 * 
+	 * @param value
+	 */
 	public void add (int value) {
 		
 	}
 	
+	
+	/** Subtracts a value from the total
+	 * 
+	 * @param value
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	
+	/** Multiplies the total by a value
+	 * 
+	 * @param value
+	 */
 	public void multiply (int value) {
 		
 	}
 	
+	
+	/** Divides the total by a value
+	 * 
+	 * @param value
+	 */
 	public void divide (int value) {
 		
 	}
 	
+	
+	/** Returns an empty string 
+	 * 
+	 * @return ""
+	 */
 	public String getHistory () {
 		return "";
 	}
---
+++ Assignment3/src/cse360assign3/Calculator_Test.java
@@ -0,0 +1,45 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Calculator_Test {
+
+	@Test
+	public void testCalculator() {
+		Calculator test_calc = new Calculator();
+		assertNotNull(test_calc);
+	}
+	
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+
+}
